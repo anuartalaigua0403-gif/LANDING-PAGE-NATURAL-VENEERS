823b188a1da576c2568719a18d268156294c28fc
anuartalaigua0403-gif <anuartalaigua0403@gmail.com>
2026-06-09 14:27:03 -0500

    add: src/types/index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export type Lang = 'es' | 'en'
+
+export interface Product {
+  id: string
+  image: string
+  titleEs: string
+  titleEn: string
+  descEs: string
+  descEn: string
+  tag: string
+}
+
+export interface ProcessStep {
+  number: string
+  titleEs: string
+  titleEn: string
+  descEs: string
+  descEn: string
+  image: string
+}
+
+export interface FAQItem {
+  questionEs: string
+  questionEn: string
+  answerEs: string
+  answerEn: string
+}
+
+export interface ContactFormData {
+  name: string
+  email: string
+  phone?: string
+  message: string
+  lang: Lang
+}
+
+export interface ContactApiResponse {
+  success: boolean
+  message: string
+}
