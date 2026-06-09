87fda733303053aa024d88b5566f0e5897a1880d
anuartalaigua0403-gif <anuartalaigua0403@gmail.com>
2026-06-09 17:50:15 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  experimental: {
-    optimizeCss: true,
-  },
-}
+}  
 
 export default nextConfig
