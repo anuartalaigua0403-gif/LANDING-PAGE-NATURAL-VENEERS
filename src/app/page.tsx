64cbe40627fe24c5b8e189ff6a9d0e9aeac37cfd
anuartalaigua0403-gif <anuartalaigua0403@gmail.com>
2026-06-09 13:51:38 -0500

    add: src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/sections/Hero'
+import Marquee from '@/components/sections/Marquee'
+import Products from '@/components/sections/Products'
+import Process from '@/components/sections/Process'
+import Results from '@/components/sections/Results'
+import Stats from '@/components/sections/Stats'
+import About from '@/components/sections/About'
+import FAQ from '@/components/sections/FAQ'
+import CTA from '@/components/sections/CTA'
+
+export default function HomePage() {
+  return (<><Hero /><Marquee /><Products /><Process /><Results /><Stats /><About /><FAQ"þ<CTA /></>)
+}
